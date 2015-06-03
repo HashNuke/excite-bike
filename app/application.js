39859ec388b2d261cc8992889c2cc8c5d3aacc48
Akash Manohar J <akash@akash.im>
2015-06-03 07:22:53 +0530

    Add button to start game

--- app/application.js
+++ app/application.js
@@ -1,11 +1,20 @@
 "use strict";
 
+var Game = require("game");
+
 var App = {
   init: function init() {
-      console.log('App initialized.');
+    console.log('App initialized.');
 
-      var gameContainer = d3.select(".game-container")
-      gameContainer.style({"background-color": "black", "height": "200px"});
+
+    let startGame = function() {
+      Game.init()
+    }
+
+    document.getElementById("start-game").addEventListener("click", startGame)
+
+    let gameContainer = d3.select(".game-container")
+    gameContainer.style({"background-color": "black", "height": "200px"});
   }
 };
 
---
+++ app/game.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var Game = {
+  init: function() {
+    console.log("started game");
+  }
+}
+
+module.exports = Game;
