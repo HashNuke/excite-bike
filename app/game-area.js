5b4d6b20312bbec747f96cb708b68fd50be56eb6
Akash Manohar J <akash@akash.im>
2015-06-05 10:27:59 +0530

    GameArea setup

--- app/game-area.js
+++ app/game-area.js
@@ -1,16 +1,21 @@
 "use strict";
 
+var GameConfig = require("game-config"),
+    Player = require("player");
+
+
 var GameArea = {
 
   init: function() {
     let gameContainer = d3.select("body")
           .insert("svg", ":first-child")
-          .attr("class", "game-container")
-          .attr("width", 480)
-          .attr("height", 320);
+          .attr("class", GameConfig.gameContainerSelector)
+          .attr("width", GameConfig.getHeight())
+          .attr("height", GameConfig.getWidth());
     gameContainer.style("background-color", "black");
+    new Player();
   }
 
-}
+};
 
 module.exports = GameArea;
