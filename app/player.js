fc984405380e89e60151943bc1a1ba9f960b7eaa
Akash Manohar J <akash@akash.im>
2015-06-05 11:57:59 +0530

    Update player.js

--- app/player.js
+++ app/player.js
@@ -1,24 +1,86 @@
 "use strict";
 
 class Player {
-  constructor(bindControls=false) {
+
+  constructor(humanPlayer=false) {
     console.log("Init player");
 
-    if(bindControls) {
-      d3.select("body").on("keydown", () => {
-        let action = this.getActionForKey(d3.event.which);
-        console.log(action);
-      });
+    this.humanPlayer = humanPlayer;
+    this.dead = false;
+    this.stopped = false;
 
-      d3.select("body").on("keyup", ()=>{
-        let action = this.getActionForKey(d3.event.which);
-        console.log(action);
-      });
+    if(humanPlayer) {
+      d3.select("body").on("keydown", this.actionForKeyDown.bind(this));
+      d3.select("body").on("keyup", this.actionForKeyUp.bind(this));
     }
   }
 
 
-  getActionForKey(key) {
+  canMove() {
+    return !this.dead && !this.stopped && this.onGround;
+  }
+
+
+  moveRight() {
+    if(!this.canMove()) return false;
+    return true;
+  }
+
+
+  moveUp() {
+    if(!this.canMove()) return false;
+    return true;
+  }
+
+
+  moveDown() {
+    if(!this.canMove()) return false;
+    return true;
+  }
+
+
+  actionForKeyDown() {
+    let actionName = this.actionNameForKey(d3.event.which);
+
+    if(actionName == "right") {
+      this.moveRight();
+      return;
+    }
+
+    if(actionName == "up") {
+      this.moveUp();
+      return;
+    }
+
+    if(actionName == "down") {
+      this.moveDown();
+      return;
+    }
+  }
+
+
+  actionForKeyUp() {
+    // TODO movement should slow down
+    let actionName = this.actionNameForKey(d3.event.which);
+
+    if(actionName == "right") {
+      // this.moveRight();
+      return;
+    }
+
+    if(actionName == "up") {
+      // this.moveUp();
+      return;
+    }
+
+    if(actionName == "down") {
+      // this.moveDown();
+      return;
+    }
+  }
+
+
+  actionNameForKey(key) {
     // up arrow or w
     if(key == 38 || key == 87) return "up";
 
@@ -26,7 +88,7 @@ class Player {
     if(key == 40 || key == 83) return "down";
 
     // left arrow or a
-    if(key == 37 || key == 65) return "left";
+    // if(key == 37 || key == 65) return "left";
 
     // right arrow or d
     if(key == 39 || key == 68) return "right";
