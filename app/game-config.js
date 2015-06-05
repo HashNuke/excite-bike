1057f4377efe61e917c058e13e395c75386a35d5
Akash Manohar J <akash@akash.im>
2015-06-05 10:27:24 +0530

    Add game config

---
+++ app/game-config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var GameConfig = {
+  gameContainerSelector: ".game-container",
+
+  getHeight: function() { return 480; },
+
+  getWidth: function() { return 320; }
+};
+
+module.exports = GameConfig;
