60dc046c7f8820ed36e07cb67e01c47776c5aa6a
Akash Manohar J <akash@akash.im>
2015-06-05 10:27:49 +0530

    Use d3 events

--- app/application.js
+++ app/application.js
@@ -7,12 +7,7 @@ var App = {
   init: function init() {
     console.log('App initialized.');
     GameArea.init();
-
-    let startGame = function() {
-      Game.init();
-    };
-
-    document.getElementById("start-game").addEventListener("click", startGame);
+    d3.select("#start-game").on("click", Game.init);
   }
 };
 
