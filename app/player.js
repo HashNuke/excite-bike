5267a1c92e18707c46ca4d6ce1863fd7f678b7ce
Akash Manohar J <akash@akash.im>
2015-06-05 09:48:30 +0530

    Add player class

---
+++ app/player.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class Player {
+  constructor() {
+    console.log("Init player");
+
+    d3.select("body").on("keydown", () => {
+      let action = this.getActionForKey(d3.event.which);
+      console.log(action);
+    });
+
+    d3.select("body").on("keyup", ()=>{
+      let action = this.getActionForKey(d3.event.which);
+      console.log(action);
+    });
+  }
+
+
+  getActionForKey(key) {
+    // up arrow or w
+    if(key == 38 || key == 87) return "up";
+
+    // down arrow or s
+    if(key == 40 || key == 83) return "down";
+
+    // left arrow or a
+    if(key == 37 || key == 65) return "left";
+    
+    // right arrow or d
+    if(key == 39 || key == 68) return "right";
+  }
+}
+
+module.exports = Player;
